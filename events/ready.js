29e7d235913e89e001ceb7c31e6c4c320be1fb02
Yangsb <ysb5397@gmail.com>
2025-10-03 14:02:45 +0900

    코드 수정

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 const { Events, ChannelType } = require('discord.js');
 const { startEarthquakeMonitor } = require('../utils/earthquake');
-const { startStatusUpdater } = require('../utils/bot_message.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
 const TARGET_CHANNEL_ID = "1353292092016693282";
@@ -14,9 +13,6 @@ module.exports = {
 
         // 지진 정보 모니터링 시작 (내부 스케줄러 사용)
         startEarthquakeMonitor(client);
-
-        // 봇 상태 메시지 업데이트 시작
-        startStatusUpdater(client);
 
         const targetChannel = await client.channels.fetch(TARGET_CHANNEL_ID).catch(() => null);
         if (targetChannel && targetChannel.type === ChannelType.GuildVoice) {
