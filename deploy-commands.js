5245158dd13c41c6982de99114bd9f0dc7e90de4
ysb5397 <ysb5397@gmail.com>
2025-10-24 07:32:44 +0000

    환경 변수 불러오는 방식 변경

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,6 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
