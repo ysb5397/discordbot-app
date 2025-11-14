ecd44f721d67642aaf5fa92effd166d55a92258a
ysb5397 <ysb5397@gmail.com>
2025-11-14 15:10:01 +0900

    역할 제거

--- commands/white_list.js
+++ commands/white_list.js
@@ -1,17 +1,21 @@
 const { SlashCommandBuilder, InteractionContextType } = require('discord.js');
 const { WhiteList } = require('../utils/database.js');
+const { content } = require('googleapis/build/src/apis/content/index.js');
+
+const OWNER_ID = process.env.MY_DISCORD_USER_ID;
+const BASE_MEMBER_ROLE_ID = process.env.BASE_MEMBER_ROLE_ID;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dev_white_list')
-        .setDescription('화리 설정을 변경 합니다.')
+        .setDescription('화리 설정을 변경 합니다. (관리자 전용)')
         .setContexts([
             InteractionContextType.Guild,          // 1. 서버
             InteractionContextType.BotDM,          // 2. 봇과의 1:1 DM
             InteractionContextType.PrivateChannel, // 3. 그룹 DM
         ])
-        .addStringOption(option =>
-            option.setName('member_id')
+        .addUserOption(option =>
+            option.setName('member')
                 .setDescription('화리에서 찾을 이용자의 ID를 넣습니다.')
                 .setRequired(true))
         .addBooleanOption(option =>
@@ -21,30 +25,41 @@ module.exports = {
 
     async execute(interaction) {
         try {
-            const memberId = interaction.options.getString('member_id');
+            const member = interaction.options.getMember('member');
             const setSafety = interaction.options.getBoolean('set_safety') || false;
+            const role = await interaction.guild.roles.fetch(BASE_MEMBER_ROLE_ID);
 
             await interaction.deferReply();
             
-            const foundMember = await WhiteList.findOne({ memberId: memberId });
+            const foundMember = await WhiteList.findOne({ memberId: member.id });
 
             if (foundMember == null) {
-                await interaction.editReply('사용자를 찾지 못했어요... 화리를 새로 추가할게요!');
+                await interaction.editReply({content: '사용자를 찾지 못했어요... 화리를 새로 추가할게요!', ephemeral: true});
 
                 const newWhiteList = new WhiteList({
-                    memberId: memberId,
+                    memberId: member.Id,
                     isWhite: setSafety
                 });
 
                 await newWhiteList.save();
-                await interaction.editReply(`화리 추가가 완료됐어요! / 추가된 멤버 ID : ${memberId}`);
+                await interaction.editReply({content: `화리 추가가 완료됐어요! / 추가된 멤버 ID : ${member.memberId}`, ephemeral: true});
                 return;
+            }
+
+            if (role == null) {
+                throw Error("역할을 찾을 수 없어요.");
+            }
+
+            if (!setSafety) {
+                await member.roles.remove(role);
+            } else {
+                await member.roles.add(role);
             }
 
             await foundMember.updateOne({
                 isWhite: setSafety
             });
-            await interaction.editReply(`화리 수정이 완료됐어요! / 수정된 멤버 ID : ${memberId}`);
+            await interaction.editReply({content: `화리 수정이 완료됐어요! / 수정된 멤버 ID : ${member.id}`, ephemeral: true});
         } catch(e) {
             console.error('whiteList 수정에 실패했어요....', e);
             throw e;
